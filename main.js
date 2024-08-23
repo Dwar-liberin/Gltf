d66ca77b2f07a6417ca055c63e59dffce86c7ff1
Liberin <165782640+Dwar-liberin@users.noreply.github.com>
2024-08-23 18:42:17 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,264 @@
+
+    import {
+      loadGLTF,
+      loadTexture,
+      loadTextures,
+      loadVideo,
+    } from "https://cdn.jsdelivr.net/gh/Dwar-liberin/dwar-lib/libs/loader.js";
+
+    import { createChromaMaterial } from "https://cdn.jsdelivr.net/gh/Dwar-liberin/dwar-lib/libs/chroma-video.js" 
+    import TWEEN from "https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.6.4/dist/tween.esm.js";
+      let analytics=null
+  const animationManager = new AnimationManager();
+
+      const isIOS = navigator.appVersion.indexOf("Mac") != -1 ? true : false;
+      try {
+
+        analytics = new Analytics(
+          {
+              appName: "DwAR",
+              customerId: "1",
+              campaignName: "gltf",
+              serverUrl: "https://lttl.in/event"
+          }
+       );
+
+      Analytics.active = true;
+
+      } catch(e) {
+        console.log("Err", e.message)
+      }
+      
+  
+
+    const THREE = window.MINDAR.IMAGE.THREE;
+
+    
+    async function loadUnmuteLogo() {
+      const muteIconGeo = new THREE.CircleGeometry(0.5, 32);
+      const muteIconTexture = await loadTexture("assets/mute.png");
+      const muteIconMaterial = new THREE.MeshBasicMaterial({
+        map: muteIconTexture,
+      });
+    
+      const muteIconMesh = new THREE.Mesh(muteIconGeo, muteIconMaterial);
+      muteIconMesh.scale.set(0.1, 0.1);
+      muteIconMesh.position.set(0, -0.5, 0.3);
+    
+      muteIconMesh.userData.clickable = true;
+    
+      return muteIconMesh;
+    }
+
+    
+
+    
+ function executeAnimation(animation, mesh) {
+  console.log('animation', animation)
+  const { name, state, value, duration, delay } = animation;
+
+  switch (name) {
+    case "slide":
+      if (state === "left") {
+        animationManager.animateSlide(mesh, null, duration, null, value, delay);
+      }
+      else if (state === "right") {
+        animationManager.animateSlide(mesh, null, duration, value, state, delay);
+      } else if (state === "up") {
+        animationManager.animateSlideUp(mesh, duration, value, delay);
+      } else {
+        animationManager.animateSlideDown(mesh, duration, value, delay);
+
+      }
+      break;
+    case "fade":
+      if (state == "in") {
+        animationManager.animateFadeIn(mesh, duration, delay);
+      } else {
+        animationManager.animateFadeOut(mesh, duration, delay);
+      }
+      break;
+    case "scale":
+      if (state == "up") animationManager.animateScaleUp(mesh, value, duration, null, delay);
+      else animationManager.animateScaleDown(mesh, value, duration, null, delay);
+      break;
+    case "bounce":
+      animationManager.animateBounce(mesh, "z", value, duration, delay); // Assuming "z" axis for bounce
+      break;
+    default:
+      console.error("Unknown animation");
+      break;
+  }
+}
+    
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+    // DwAR Analytics
+    try {
+      console.log("ana", analytics)
+      if(analytics){
+        console.log("ana", analytics)
+        analytics.trackPageLoad({
+          eventType: 'load',
+          payload: true,
+        });
+  
+        analytics.sendQueryParam()
+      }
+     
+    } catch(e) {
+      console.log("e", e.message)
+    }
+
+
+    async function start(){
+
+      let muteIconMesh;
+      
+      
+ 
+  const mindThree =  new window.MINDAR.IMAGE.MindARThree({
+    container: document.body,
+    imageTargetSrc: "assets/target.mind",
+    uiLoading: "#scanning-overlay",
+  });
+
+  const { renderer, scene, camera } = mindThree;
+  const anchor = mindThree.addAnchor(0);
+
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  scene.add(light);
+
+  const loadFont = (fontURL) => {
+    return new Promise((resolve, reject) => {
+      const loader = new THREE.FontLoader();
+
+      loader.load(
+        fontURL,
+        (font) => {
+          resolve(font); // Resolve the promise with the loaded font
+        },
+        undefined, // Progress callback (optional)
+        (error) => {
+          reject(error); // Reject the promise with the error
+        }
+      );
+    });
+  };
+
+  
+    
+      
+   const gltf_bc1713db_0aed0aed2 = await loadGLTF(
+      "assets/DamagedHelmet.glb"
+    );
+     gltf_bc1713db_0aed0aed2.scene.scale.set(0.2, 0.2, 0.2);
+     gltf_bc1713db_0aed0aed2.scene.position.set(0.094, -0.06, 0.184);
+     gltf_bc1713db_0aed0aed2.scene.rotation.set(0, 0, 0);
+    
+const target_imageabout.22829_iconGeometry = new THREE.PlaneGeometry(1, 0.49850746268656715);
+   const target_imageabout.22829_texture = await loadTexture("assets/about.jpg");
+  const target_imageabout.22829_image = new THREE.MeshBasicMaterial({
+      map: target_imageabout.22829_texture,
+      transparent: true,
+      side: THREE.DoubleSide,
+    });
+    const target_imageabout.22829 = new THREE.Mesh(target_imageabout.22829_iconGeometry, target_imageabout.22829_image);
+    target_imageabout.22829.scale.set(1, 1, 1);
+    target_imageabout.22829.position.set(0.01, -0.01, 0.01);
+    target_imageabout.22829.rotation.set(-0.001, 0, 0);
+    
+    
+    
+      
+       document.body.addEventListener("click", (e) => {
+    const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
+    const mouseY = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    const mouse = new THREE.Vector2(mouseX, mouseY);
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children, true);
+
+    if (intersects.length > 0) {
+      let o = intersects[0].object;
+
+      while (o.parent && !o.userData?.clickable) {
+        o = o.parent;
+      }
+
+      if(o.userData.clickable && analytics){
+        
+        try {
+          analytics.trackClick({
+            eventType: "click",
+            payload: o.userData.eventName
+          })
+        } catch (err){
+          console.log("Err", err)
+        }
+       
+      }
+
+        if(isIOS){ 
+          if (o.userData.clickable && o === muteIconMesh) {
+            
+    
+            anchor.group.remove(muteIconMesh);
+            return true;
+          }
+        }
+
+
+      // if button is clickable then loading screen is show. And when user redirect to the screen loader automatically gone because script is change.
+      
+      if (o.userData.clickable) window.showLoadingScreen();
+
+      
+      }
+
+    })
+    
+      
+    anchor.group.add(gltf_bc1713db_0aed0aed2.scene)
+
+
+
+    anchor.onTargetFound = () => {
+      try {
+        if(analytics){
+          analytics.trackMarkerScanned();
+        }
+      } catch(e) {
+        console.log("Err", e)
+      }
+
+            
+
+
+     
+      
+    };
+
+
+    anchor.onTargetLost = () => {
+      
+
+        
+
+    }
+    
+    
+      
+    await mindThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+       TWEEN.update();
+    });
+    
+    }
+    start();
+    })
+    
+    
